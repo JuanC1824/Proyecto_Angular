a7f9cdd999e425d06bf551661042739d2d9611de
JuanC1824 <juanjosecardenas142006@gmail.com>
2025-09-19 14:10:50 -0500

    Nuevos Cambios

--- src/app/components/ayuda/ayuda.ts
+++ src/app/components/ayuda/ayuda.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Nav } from '../nav/nav';
 
 @Component({
   selector: 'app-ayuda',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [RouterModule, Nav],
   templateUrl: './ayuda.html',
   styleUrl: './ayuda.css'
 })
--- src/app/components/calendario/calendario.ts
+++ src/app/components/calendario/calendario.ts
@@ -3,12 +3,20 @@ import { RouterModule } from '@angular/router';
 import { Nav } from '../nav/nav';
 import { Footer } from '../footer/footer';
 
+type Estado = 'disponible' | 'agotado' | 'espera';
+
 @Component({
   selector: 'app-calendario',
   imports: [RouterModule, Nav, Footer],
   templateUrl: './calendario.html',
   styleUrls: ['./calendario.css']
 })
+
 export class Calendario {
 
+  estado: Estado = 'disponible';
+
+  cambiarEstado(nuevoEstado: Estado) {
+    this.estado = nuevoEstado;
+  }
 }
